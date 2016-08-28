462e3fbc9f739c70285905cab7c4ab475d158c9f
SaraAnn Stanway <sarastanway@gmail.com>
2016-08-28 00:43:27 -0400

    fucking stupid goddamn dynamically typed languages

--- app/views.py
+++ app/views.py
@@ -10,9 +10,11 @@ def index():
 		short_url = request.form["short"]
 		modified_long_url = validate(long_url)
 		if modified_long_url != None and len(short_url) < 11:
-			return render_template("confirm.html", longurl = modified_long_url, shorturl = short_url)
 			new_url = Pairs(long = modified_long_url, short = short_url)
 			new_url.save()
+			return modified_long_url + " " + short_url
+			
+			#return render_template("confirm.html", longurl = modified_long_url, shorturl = short_url)
 		else:
 			return render_template("error.html")
 	
@@ -20,11 +22,11 @@ def index():
 		return render_template("base.html")
 
 @app.route('/short/<website>', methods =['GET'])
-def shortcut(website): 
+def shortcut(website):
 	if request.method == 'GET':
 		#return render_template("error.html")
 		website = Pairs.objects()
-		#return render_template("data.html", saved = "website")#redirect(website)
+		return render_template("data.html", saved = website)#redirect(website)
 
 @app.route('/confirm', methods = ['GET'])
 def confirm():
