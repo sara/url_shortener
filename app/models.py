c52107522dda7485205b56305f9d82c4aac1d170
SaraAnn Stanway <sarastanway@gmail.com>
2016-08-29 20:23:34 -0400

    WOOO QUERIES AND REDIRECTS WORK

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from app import db
 
 class Pairs (db.Document):
-	long = db.StringField(required = True)
+	full = db.StringField(required = True)
 	short = db.StringField(required = True)
 	#set unique to true once you get the basic working
