15e0e4ba507f5877a2afc62e1b7ae8e107920d2a
SaraAnn Stanway <sarastanway@gmail.com>
2016-08-29 20:26:53 -0400

    WOOO QUERIES AND REDIRECTS WORK

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ def index():
 		short_url = request.form["short"]
 		modified_long_url = validate(long_url)
 		if modified_long_url != None and len(short_url) < 11:
-			new_url = Pairs(long = modified_long_url, short = short_url)
+			new_url = Pairs(full = modified_long_url, short = short_url)
 			new_url.save()
 			return modified_long_url + " " + short_url
 			
@@ -25,9 +25,12 @@ def index():
 def shortcut(website):
 	if request.method == 'GET':
 		#return render_template("error.html")
-		website = Pairs.objects()
-		return render_template("data.html", saved = website)#redirect(website)
-
+		#website = Pairs.objects()
+		merp = Pairs.objects(short = website)
+		target = merp[0].full
+		#return target#merp[0].full
+		#target = merp.long
+		return redirect(target)
 @app.route('/confirm', methods = ['GET'])
 def confirm():
 	if request.method =='GET':
