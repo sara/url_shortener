1c82ba476f12050f6a536f0e8fda28f90d84dae5
SaraAnn Stanway <sarastanway@gmail.com>
2016-08-30 20:34:19 -0400

    added url generation, modified error page

--- app/views.py
+++ app/views.py
@@ -2,21 +2,23 @@ from app import app, db
 from flask import Flask, flash, render_template, request, redirect, url_for
 from urllib2 import urlopen
 from models import Pairs
-
+import random
+from random import choice
+from string import ascii_lowercase
 @app.route('/', methods = ['GET', 'POST'])
 def index():
 	if request.method =='POST':
 		long_url = request.form["original"]
 		short_url = request.form["short"]
+		if len(short_url) == 0:
+			short_url = make_short()
 		modified_long_url = validate(long_url)
 		if modified_long_url != None and len(short_url) < 11:
 			new_url = Pairs(full = modified_long_url, short = short_url)
 			new_url.save()
 			return modified_long_url + " " + short_url
-			
-			#return render_template("confirm.html", longurl = modified_long_url, shorturl = short_url)
 		else:
-			return redirect(url_for('.error'))#render_template("error.html")
+			return redirect(url_for('.error'))
 	
 	if request.method == 'GET':
 		return render_template("homepage.html")
@@ -31,12 +33,8 @@ def error():
 @app.route('/short/<website>', methods =['GET'])
 def shortcut(website):
 	if request.method == 'GET':
-		#return render_template("error.html")
-		#website = Pairs.objects()
 		merp = Pairs.objects(short = website)
 		target = merp[0].full
-		#return target#merp[0].full
-		#target = merp.long
 		return redirect(target)
 @app.route('/confirm', methods = ['GET'])
 def confirm():
@@ -46,7 +44,6 @@ def confirm():
 
 #check if if's a real url
 def validate(long_url):
-	#EXTRA: add on http/https to input
 	try:
 		file = urlopen(long_url)
 		modified_long_url = file.geturl()
@@ -54,3 +51,9 @@ def validate(long_url):
 		modified_long_url = None
 		return None
 	return modified_long_url
+
+def make_short():
+	little = ''
+	for i in range (5):
+		little += choice(ascii_lowercase)
+	return little
